835cfecd1ec083174393335b731d185a1c6ac976
Francisco <franciscogssm94@gmail.com>
2019-09-21 18:39:58 -0300

    Adicionado pasta com firmwares dos modulos

---
+++ Scripts/Firmware/DAq_Master/Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.d
@@ -0,0 +1,80 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_hal.c \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Inc/stm32f1xx_hal_conf.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Include/core_cm3.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Inc/stm32f1xx_hal_conf.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Include/core_cm3.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+
+C:/codeCave/03-TCC/Scripts/Firmware/DAq_Master/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
