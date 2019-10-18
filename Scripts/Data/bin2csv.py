0f6b7db63197c74fcb0d23f04fa0350557c1caa5
Francisco <franciscogssm94@gmail.com>
2019-10-18 00:56:33 -0300

    Implementacao do blink, da leitura de offset e das relacoes bits por mg de cada canal e salvou-se no arquivo que esta sendo gravado. Processamento do arquivo ok. O maior problema ainda esta sendo filtrar o sinal, mas aparentemente a relacao sinal ruido esta satisfatoria apos filtrado. Algumas melhorias que podem ser feitas: biblioteca do nrf pouco otimizada no envio de SPI. Quando se tem muitos envios, poderia se implementar para enviar tudo de uma vez. Poderia-se tambem tracar um perfil do ruido, igual o audacity faz. Da mesma forma que se faz uma amostragem para pegar o offset, poderia se fazer o mesmo para o padrao do ruido.

--- Scripts/Data/bin2csv.py
+++ Scripts/Data/bin2csv.py
@@ -14,19 +14,18 @@ if(not os.path.exists(os.path.dirname(outputPath))):
 _fileOutput = False
 filesCounter = 0
 runnedOnce = False
-accs = [[],[],[],[]]
+acc = [[],[],[],[]]
 accData = ''
 
+bits2g = [0,0,0,0]
+offset = [0,0,0,0]
+
 overwriteFiles = False
-offsetRemove = False
 
 if(len(sys.argv) > 1):
     if("--overwrite" in sys.argv):
         overwriteFiles = True
         print("Arquivos ja existentes serao sobrescritos")
-    if("--offset" in sys.argv):
-        offsetRemove = True
-        print("Os arquivos serao gravados sem offset")
     
 # r=root, d=directories, f = files
 for r,d,f in os.walk(path):
@@ -54,10 +53,25 @@ for r,d,f in os.walk(path):
                         _fileOutput = False
                         filesCounter = 0
                         accData = ''
+                        acc = [[],[],[],[]]
 
                     filesCounter = filesCounter + 1
                     print("Processando arquivo: " + file)
                     
+                    boardInfo = data.split(",", 8)
+
+                    bits2g[0] = float(boardInfo[0])*10     #100mg/V
+                    bits2g[1] = float(boardInfo[1])*10
+                    bits2g[2] = float(boardInfo[2])*10
+                    bits2g[3] = float(boardInfo[3])*10
+
+                    offset[0] = float(boardInfo[4])
+                    offset[1] = float(boardInfo[5])
+                    offset[2] = float(boardInfo[6])
+                    offset[3] = float(boardInfo[7])
+
+                    data = boardInfo[8]
+
                     for i in range(0, len(data), 32):
                         if(i != 0):
                             
@@ -74,33 +88,41 @@ for r,d,f in os.walk(path):
                             prevPackageIndex = ((ord(data[31]) & 0xFF) << 4)
 
                         for j in range(nPackagesLost + 1):
-                            accData += str((ord(data[1 + i]) & 0xFF) + ((ord(data[2 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[2 + i]) & 0xF0)>>4) + ((ord(data[3 + i]) & 0xFF)<<4)) + '\t'
-                            accData += str((ord(data[4 + i]) & 0xFF) + ((ord(data[5 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[5 + i]) & 0xF0)>>4) + ((ord(data[6 + i]) & 0xFF)<<4)) + '\n'
+                            acc[0].append((ord(data[1 + i]) & 0xFF) + ((ord(data[2 + i]) & 0x0F)<<8))
+                            acc[1].append(((ord(data[2 + i]) & 0xF0)>>4) + ((ord(data[3 + i]) & 0xFF)<<4))
+                            acc[2].append((ord(data[4 + i]) & 0xFF) + ((ord(data[5 + i]) & 0x0F)<<8))
+                            acc[3].append(((ord(data[5 + i]) & 0xF0)>>4) + ((ord(data[6 + i]) & 0xFF)<<4))
 
-                            accData += str((ord(data[7 + i]) & 0xFF) + ((ord(data[8 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[8 + i]) & 0xF0)>>4) + ((ord(data[9 + i]) & 0xFF)<<4)) + '\t'
-                            accData += str((ord(data[10 + i]) & 0xFF) + ((ord(data[11 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[11 + i]) & 0xF0)>>4) + ((ord(data[12 + i]) & 0xFF)<<4)) + '\n'
+                            acc[0].append((ord(data[7 + i]) & 0xFF) + ((ord(data[8 + i]) & 0x0F)<<8))
+                            acc[1].append(((ord(data[8 + i]) & 0xF0)>>4) + ((ord(data[9 + i]) & 0xFF)<<4))
+                            acc[2].append((ord(data[10 + i]) & 0xFF) + ((ord(data[11 + i]) & 0x0F)<<8))
+                            acc[3].append(((ord(data[11 + i]) & 0xF0)>>4) + ((ord(data[12 + i]) & 0xFF)<<4))
 
-                            accData += str((ord(data[13 + i]) & 0xFF) + ((ord(data[14 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[14 + i]) & 0xF0)>>4) + ((ord(data[15 + i]) & 0xFF)<<4)) + '\t'
-                            accData += str((ord(data[16 + i]) & 0xFF) + ((ord(data[17 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[17 + i]) & 0xF0)>>4) + ((ord(data[18 + i]) & 0xFF)<<4)) + '\n'
+                            acc[0].append((ord(data[13 + i]) & 0xFF) + ((ord(data[14 + i]) & 0x0F)<<8))
+                            acc[1].append(((ord(data[14 + i]) & 0xF0)>>4) + ((ord(data[15 + i]) & 0xFF)<<4))
+                            acc[2].append((ord(data[16 + i]) & 0xFF) + ((ord(data[17 + i]) & 0x0F)<<8))
+                            acc[3].append(((ord(data[17 + i]) & 0xF0)>>4) + ((ord(data[18 + i]) & 0xFF)<<4))
 
-                            accData += str((ord(data[19 + i]) & 0xFF) + ((ord(data[20 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[20 + i]) & 0xF0)>>4) + ((ord(data[21 + i]) & 0xFF)<<4)) + '\t'
-                            accData += str((ord(data[22 + i]) & 0xFF) + ((ord(data[23 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[23 + i]) & 0xF0)>>4) + ((ord(data[24 + i]) & 0xFF)<<4)) + '\n'
+                            acc[0].append((ord(data[19 + i]) & 0xFF) + ((ord(data[20 + i]) & 0x0F)<<8))
+                            acc[1].append(((ord(data[20 + i]) & 0xF0)>>4) + ((ord(data[21 + i]) & 0xFF)<<4))
+                            acc[2].append((ord(data[22 + i]) & 0xFF) + ((ord(data[23 + i]) & 0x0F)<<8))
+                            acc[3].append(((ord(data[23 + i]) & 0xF0)>>4) + ((ord(data[24 + i]) & 0xFF)<<4))
 
-                            accData += str((ord(data[25 + i]) & 0xFF) + ((ord(data[26 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[26 + i]) & 0xF0)>>4) + ((ord(data[27 + i]) & 0xFF)<<4)) + '\t'
-                            accData += str((ord(data[28 + i]) & 0xFF) + ((ord(data[29 + i]) & 0x0F)<<8)) + '\t'
-                            accData += str(((ord(data[29 + i]) & 0xF0)>>4) + ((ord(data[30 + i]) & 0xFF)<<4)) + '\n'
+                            acc[0].append((ord(data[25 + i]) & 0xFF) + ((ord(data[26 + i]) & 0x0F)<<8))
+                            acc[1].append(((ord(data[26 + i]) & 0xF0)>>4) + ((ord(data[27 + i]) & 0xFF)<<4))
+                            acc[2].append((ord(data[28 + i]) & 0xFF) + ((ord(data[29 + i]) & 0x0F)<<8))
+                            acc[3].append(((ord(data[29 + i]) & 0xF0)>>4) + ((ord(data[30 + i]) & 0xFF)<<4))
 
                         nPackagesLost = 0
 
+                    #Convert to g
+                    for i in range(len(acc[0])):
+                        acc[0][i] = (acc[0][i] - offset[0])*bits2g[0]
+                        acc[1][i] = (acc[1][i] - offset[1])*bits2g[1]
+                        acc[2][i] = (acc[2][i] - offset[2])*bits2g[2]
+                        acc[3][i] = (acc[3][i] - offset[3])*bits2g[3]
+                        accData += str(acc[0][i]) + "\t" + str(acc[1][i]) + "\t" + str(acc[2][i]) + "\t" + str(acc[3][i]) + "\n" 
+                        
                     if(not _fileOutput):
                         _fileOutput = open(outputPath + file, "w")
 
